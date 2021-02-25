1c6ce1bfd74dc39d2ffd6dec67e0f11b74d0fafc
timstuf <nastushagrishaeva@gmail.com>
2021-02-25 20:19:53 +0200

    model fix

--- src/com/company/model/Car.java
+++ src/com/company/model/Car.java
@@ -1,7 +1,13 @@
 package com.company.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Car {
-    public List<String> streets;
+    public List<Street> streets;
+
+    public List<Intersection> getCarIntersections()
+    {
+        return streets.stream().map(x->x.endIntersection).collect(Collectors.toList());
+    }
 }
