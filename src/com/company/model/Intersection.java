14c3b34ff326cf1ccb104748bc2cbcb9f0b83d31
Konstantin <kostiantyn.stadnyk@techs.com.ua>
2021-02-25 20:38:37 +0200

    constructors

--- src/com/company/model/Intersection.java
+++ src/com/company/model/Intersection.java
@@ -1,5 +1,12 @@
 package com.company.model;
 
 public class Intersection {
+
+    public Intersection(int id, int greenLight) {
+        this.id = id;
+        this.greenLight = greenLight;
+    }
+
     public int id;
+    public int greenLight;
 }
--- src/com/company/model/Street.java
+++ src/com/company/model/Street.java
@@ -1,6 +1,13 @@
 package com.company.model;
 
 public class Street {
+    public Street(Intersection startIntersection, Intersection endIntersection, String name, int length) {
+        this.startIntersection = startIntersection;
+        this.endIntersection = endIntersection;
+        this.name = name;
+        this.length = length;
+    }
+
     public Intersection startIntersection;
     public Intersection endIntersection;
     public String name;
